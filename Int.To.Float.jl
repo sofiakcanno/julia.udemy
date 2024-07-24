3e8b9cd4a8328dfd34ce2c45b1469286b0227e7a
Soffi Kcanno <151199753+sofiakcanno@users.noreply.github.com>
2024-07-24 19:09:47 +0200

    Add files via upload

---
+++ Int.To.Float.jl
@@ -0,0 +1,8 @@
+myint = 42
+
+# Convert myint to a float and assign it to myfloat
+myfloat = Float64(myint)
+
+# Print the results to verify
+println("myint is: ", myint)
+println("myfloat is: ", myfloat)
